ab6900d6ab840f061e2b8a2f94015e74bd1e2b71
Muhammad Muhibbudin <muh.muhibudin@gmail.com>
2025-02-16 23:46:27 +0700

    awalan

---
+++ index.php
@@ -0,0 +1,30 @@
+<h1>TODO LIST HARI INI</h1>
+
+<form action"">
+    <label>Apa kegiatanmu hari ini ? </label><br>
+    <input type="text" name="todo">
+    <button type="submit"></button>
+</form>
+
+<ul>
+    <li>
+        <input type="checkbox" name"todo[]">
+        <label>Tugas hari ini</label>
+        <a href="#">Hapus</a>
+    </li>
+    <li>
+        <input type="checkbox" name"todo[]">
+        <label>Tugas hari ini</label>
+        <a href="#">Hapus</a>
+    </li>
+    <li>
+        <input type="checkbox" name"todo[]">
+        <label>Tugas hari ini</label>
+        <a href="#">Hapus</a>
+    </li>
+    <li>
+        <input type="checkbox" name"todo[]">
+        <label>Tugas hari ini</label>
+        <a href="#">Hapus</a>
+    </li>
+</ul>
