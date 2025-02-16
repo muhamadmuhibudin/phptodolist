e456025ae476ba6fe73f456fc4d75ac7efb042c9
Muhammad Muhibbudin <muh.muhibudin@gmail.com>
2025-02-16 23:57:59 +0700

    tambahan fungsi menambahkan data baru

--- index.php
+++ index.php
@@ -1,7 +1,26 @@
+<?php
+
+$todos = [];
+$file = file_get_contents('todo.txt');
+$todos = unserialize($file);
+
+//fungsi untuk melihat apakah data sudah ada update, ketika ada maka dia akan membaca data terbaru (tanpa hapus)
+
+if(isset($POST['todo'])){
+    $data = $POST['todo']
+    $todos[] = [
+        'todo' => $data
+        'status' => 0
+    ];
+    file_put_contents('todo.txt'.serialize($todos));
+}
+
+?>
+
 <h1>TODO LIST HARI INI</h1>
 
-<form action"">
-    <label>Apa kegiatanmu hari ini ? </label><br>
+<form method="POST">
+    <label> Apa kegiatanmu hari ini ? </label> <br>
     <input type="text" name="todo">
     <button type="submit"></button>
 </form>
