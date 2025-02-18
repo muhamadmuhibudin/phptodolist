c635e86ae813688a637c4ca9e729a85880cee6bf
Muhammad Muhibbudin <muh.muhibudin@gmail.com>
2025-02-18 11:42:01 +0700

    tambahan fungsi menambah

--- index.php
+++ index.php
@@ -2,6 +2,10 @@
 
 //total array yang disiapkan untuk disimpan
 $todos	= []; 
+if(file_exists('todo.txt')){
+$file = file_get_contents('todo.txt');
+$todos = unserialize($file);
+}
 
 //Jika ditemukan todo yang dikirim melalui methode POST
 
@@ -13,8 +17,9 @@ if(isset($_POST['todo']))
                 'status'=>0
     		  ];
 	$daftar_belanja=serialize($todos);//simpan daftar belanja dalam format serialized
-    file_put_contents('todo.txt',$daftar_belanja);
+    file_put_contents('todo.txt',serialize($todos));
 }
+
 ?>
 
 <h1>TODO LIST HARI INI</h1>
@@ -22,28 +27,16 @@ if(isset($_POST['todo']))
 <form method="POST">
     <label> Apa kegiatanmu hari ini ? </label> <br>
     <input type="text" name="todo">
-    <button type="submit"></button>
+    <button type="submit">SIMPAN</button>
 </form>
 
 <ul>
+    <?php foreach($todos as $key => $value): ?>
     <li>
-        <input type="checkbox" name"todo[]">
-        <label>Tugas hari ini</label>
+        <input type="checkbox" name="todo">
+        <label><?php echo $value['todo']; ?></label>
         <a href="#">Hapus</a>
     </li>
-    <li>
-        <input type="checkbox" name"todo[]">
-        <label>Tugas hari ini</label>
-        <a href="#">Hapus</a>
-    </li>
-    <li>
-        <input type="checkbox" name"todo[]">
-        <label>Tugas hari ini</label>
-        <a href="#">Hapus</a>
-    </li>
-    <li>
-        <input type="checkbox" name"todo[]">
-        <label>Tugas hari ini</label>
-        <a href="#">Hapus</a>
-    </li>
+    <?php endforeach; ?>
+   
 </ul>
